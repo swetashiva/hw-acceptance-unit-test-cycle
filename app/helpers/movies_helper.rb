1e3dbb1a4ade17c00f24feaf742523e5a97edbfb
EC2 Default User <ec2-user@ip-172-31-30-60.us-east-2.compute.internal>
2019-03-07 23:45:15 +0000

    Completed part 1,2,3 and checked coverage

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,6 +1,6 @@
 module MoviesHelper
-  # Checks if a number is odd:
- # def oddness(count)
- #    count.odd? ?  "odd" :  "even"
-#  end
+ #Checks if a number is odd:
+  def oddness(count)
+     count.odd? ?  "odd" :  "even"
+  end
 end
